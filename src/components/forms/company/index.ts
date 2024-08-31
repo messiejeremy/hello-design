547dc969a13ccd915f1b862fac187f01d78da5ce
messiejeremy <jeremy9451.lmd@gmail.com>
2024-08-30 19:50:49 -0500

    src/components/forms/company/index.ts line deleted

--- src/components/forms/company/index.ts
+++ src/components/forms/company/index.ts
@@ -1,4 +1,3 @@
 export { CompanyFields } from './CompanyFields'
 export { CompanyForm } from './CompanyForm'
-export { CompanyInfoForm } from './CompanyInfoForm'
 export { UploadPhoto } from './UploadPhoto'
